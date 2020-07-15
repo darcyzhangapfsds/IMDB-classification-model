df3eb337ff402fa42f61170253aff01270e5fcac
darcyzhangapfsds <67884844+darcyzhangapfsds@users.noreply.github.com>
2020-07-15 12:49:10 -0600

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
-
-print(train_data[0])
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 word_index = data.get_word_index()
 
@@ -17,7 +17,6 @@ word_index["<UNK>"] = 2
 word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-
 
 train_data = keras.preprocessing.sequence.pad_sequences(
     train_data,
@@ -30,15 +29,64 @@ test_data = keras.preprocessing.sequence.pad_sequences(
     padding="post",
     maxlen=250)
 
-print(len(train_data), len(test_data))
-
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
 # MODEL
 
-model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1d())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1, activation="sigmoid"))
+model = keras.Sequential([
+    keras.layers.Embedding(encoder.vocab_size, 64),
+    keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(1)
+
+model.summary()
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+    )
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitmodel = model.fit(
+    x_train,
+    y_train,
+    epochs=40,
+    batch_size=512,
+    validation_data=(x_val, y_val),
+    verbose=1
+    )
+
+history = fitmodel
+
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+
+test_review = test_data[0]
+predict = model.predict([test_review])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: " + str(predict[0]))
+print("Actual: " + str(test_labels[0]))
+
+def plot_graphs(history, metric):
+  plt.plot(history.history[metric])
+  plt.plot(history.history['val_'+metric], '')
+  plt.xlabel("Epochs")
+  plt.ylabel(metric)
+  plt.legend([metric, 'val_'+metric])
+  plt.show()
+
+print(predict[0])
+
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
